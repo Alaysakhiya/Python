e4ea8bde01e17e16b7033d90c2ceb86c117eedce
Alay <alaysakhiya525@gmail.com>
2026-06-04 11:45:21 +0530

    practie

---
+++ practice/practice.py
@@ -0,0 +1,100 @@
+# age= int(input("Enter the age :"))
+
+# if age>=18:
+#     print("you can vote")
+# else:
+#     print("you cna not vote")
+
+# print("programme is ended")
+
+# num = int(input("Enter the number: "))
+
+# if num>0 and num%2 ==0:
+#     print(f"{num} is a even number")
+    
+# else:
+#     print(f"{num} is odd number")
+
+# text = input("enter some text:")
+
+# if text:
+#     print("you enter the :",text)
+# else:
+#     print("you enter empty string")
+
+# mark=int(input("Enter the mark :"))
+
+# if mark >=90:
+#     print("Grade = A")
+# elif mark >=75:
+#     print("Grade = B")
+# elif mark >=60:
+#     print("Grade = C")
+# elif mark >=45:
+#     print("Grade = D")
+# else:
+#     print("Grade = F")
+
+# Eng = 45
+
+# maths = 5
+
+# science =8
+
+
+
+
+# if Eng>= 50:
+#     if maths>=50:
+#         if science>=50:
+#             print("You passed all subjects")
+
+       
+#         else:
+#             print("you failed in science")
+#     else:
+#         print("you  failed in maths")
+# else:
+#     print("you failed in english")        
+
+
+# num =int(input("Enter the num :"))
+
+# result = "Even"if num%2 ==0 else"odd"
+
+# print(result)
+
+# eng = 78
+# math=78
+# sci=78
+
+# if eng>math:
+#     if eng>sci:
+#         print("eng")
+#     else:
+#         print("sci")
+# else:
+#     if math>sci:
+#         print("math")
+#     else:
+#         print("sci")                
+
+
+# print("enter 1 to tea")
+# print("Enter 2 to samosa")
+# print("Enter 3 to pizza")
+
+
+# choice=int(input("\n\nEnter the number to choice : "))
+
+# match choice:
+#     case 1:
+#         print("you order the tea")
+#     case 2:
+#         print("you order the samosa")
+#     case 3:
+#         print("You order the pizza")
+#     case _:
+#         print("Invalid")    
+
+

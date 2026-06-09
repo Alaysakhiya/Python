610b302be03c83b8e625993ce57ba1b7449af4eb
Alay <alaysakhiya525@gmail.com>
2026-06-09 10:27:11 +0530

    Logic box

---
+++ Project/PR2/LogicBox.py
@@ -0,0 +1,9 @@
+print("Welcome to the Pattern Generator and Number Analyzer !")
+
+
+while True:
+
+    print("Select the option: ")
+    print(1," To Generate the pattern")
+    print(2," To Analyze Range of Number ")
+    print(3," To Exit")

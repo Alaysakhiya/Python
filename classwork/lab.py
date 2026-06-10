1a92f49bdcbb36a18a85c0bc2e28bd8990884b51
Alay <alaysakhiya525@gmail.com>
2026-06-10 11:31:40 +0530

    lab work

---
+++ classwork/lab.py
@@ -0,0 +1,22 @@
+
+                    # lab 3.1 : 1
+# first_name=str(input("Enter the your first name : "))
+# last_name=str(input("Enter the your last name : "))
+
+# print("Hello",[first_name],[last_name], "!")
+
+                    # lab 3.1 : 2
+
+# item=str(input("Enter the name of the item : "))
+# prize= input("Enter the prize of item :")
+
+
+# print(f"The prize of {item} is {prize} rupees")
+
+
+                    # lab 3.1: 3
+
+# a=str(input("Enter the some word : "))
+a="alay"
+
+print(a[::-1])
--- practice/practice.py
+++ practice/practice.py
@@ -348,12 +348,18 @@
 #     for j in range(1,11):
 #         print(i,"X",j,"=",i*j)
 
-for i in range(5,0,-1):
-    for j in range(5,i-1,-1):
-        print(j,end=" " )
-    print()
+# for i in range(5,0,-1):
+#     for j in range(5,i-1,-1):
+#         print(j,end=" " )
+#     print()
 
-for i in range(5,0,-1):
-    for j in range(i,6,):
-        print(j,end=" ")
-    print()
+# for i in range(5,0,-1):
+#     for j in range(i,6,):
+#         print(j,end=" ")
+#     print()
+
+a ="alay sakhiya"
+b="sakhiya"
+
+
+print(a.replace("alay" ,"hello"))

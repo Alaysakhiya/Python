9fddf4972e5e541199531fb13552c416e23184e4
Alay <alaysakhiya525@gmail.com>
2026-06-11 11:48:20 +0530

    lab

---
+++ classwork/lab3.py
@@ -0,0 +1,41 @@
+
+#              lab 3.2
+
+
+# list=["appel","orenges","grapes","watermeloon","banana"]
+
+
+# # print(list[1])
+# # print(list[-1])
+
+# list.append("mango")
+# list.remove("appel")
+
+# print(list)
+
+
+# list.sort()
+# print(list)
+
+# list.reverse()
+
+# print(list)
+
+# list=["appel","orenges","grapes","watermeloon","banana"]
+
+# li=[0,3]
+
+# for i in li:
+#     print(list)
+
+
+li=[32,92,61,16,32]
+
+
+for i in li:
+    if i==32:
+        li.remove(32)
+        print(li)
+
+
+# print(li)

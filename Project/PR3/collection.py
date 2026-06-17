2d7a4a4e920697cdbe16e66f0a6944713942eeb0
Alay <alaysakhiya525@gmail.com>
2026-06-17 13:44:23 +0530

    collection manipulator

--- Project/PR3/collection.py
+++ Project/PR3/collection.py
@@ -1,6 +1,6 @@
 print("\nWelcome to the Student Data Organizer ! ")
 
-student=[ ]
+student=[]
 while True:
 
     print('''\nSelect the choice
@@ -19,11 +19,12 @@ while True:
         print("\nEnter the Student Details: ")
         
         st=    {"Student Id" : (len(student)+1),
-             "Name" : input("Name : ") ,
-             "Age" : int(input("Age : ")),
-             "Grade" : input("Grade : "),
-             "Date of Birth" : tuple(input("Date of Birth(DD-MM-YYYY) : ")),
-             "Subject" : set(input("Subject seprated by comma(,) : ").split(","))}
+                "Name" : input("Name : ") ,
+                "Age" : int(input("Age : ")),
+                "Grade" : input("Grade : "),
+                "Date of Birth" : tuple(input("Date of Birth(DD-MM-YYYY) : ")),
+                "Subject" : set(input("Subject seprated by comma(,) : ").split(","))
+                }
 
                 
         student.append(st)
@@ -32,6 +33,61 @@ while True:
     elif choice==2:
 
         for st in student:
-            print(f"ID : {st["Student Id"]}| Name : {st["Name"]}| Age : {st["Age"]}| Grade : {st["Grade"]} | Date of Birth : {(" ").join(st["Date of Birth"]) }|Subject : {(" ").join(st["Subject"])}")
+            print(f"ID : {st["Student Id"]} | Name : {st["Name"]} | Age : {st["Age"]} | Grade : {st["Grade"]} | Date of Birth : {(" ").join(st["Date of Birth"])} | Subject : {(" ").join(st["Subject"])}")
 
         
+    elif choice==3:
+
+        id=int(input("Enter ID to update the information of student : "))
+        found=False
+
+        for st in student:
+                if id==st["Student Id"]:
+                    found=True
+                    st["Name"]= input("Name : ")
+                    st["Age"] = int(input("Age : "))
+                    st["Subject"]=set(input("Subject seprated by comma(,) : ").split(","))
+                    st["Grade"] = input("Grade : ")
+
+                    print("\nStudent is updated successfully !")
+
+        if found==False:
+            print("\nId is not found !")        
+
+        
+    elif choice==4:
+
+        id=int(input("Enter ID to update the information of student : "))
+        found=False
+
+        for st in student:
+
+            if id==st["Student Id"]:
+                found=True
+                student.remove(st)
+                print("Student Deleted successfully !")
+
+        if found==False:
+            print("\nId is not found !") 
+
+
+    elif choice==5:
+
+        if len(student)==0:
+            print("\nStudent not found")
+        else:
+            print("\nSubjects are : ")
+            for st in student:
+                print(f"Name : {st["Name"]} | Subject : {(" ").join(st["Subject"])}")
+
+    elif choice==0:
+
+        print("\nProgramme is Closed !")
+        break
+
+    else:
+        print("\nInvalid !")
+
+            
+
+

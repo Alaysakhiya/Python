8bb26d431264976ab65264c6fbdbd16dce4cfbaa
Alay <alaysakhiya525@gmail.com>
2026-08-25 16:40:28 +0530

    file hande

--- File handling/file.py
+++ File handling/file.py
@@ -1,4 +1,8 @@
-a = open("D:\\Python\\File handling\\file1.txt")
 
-# print(a.readline())
-print(a.read())
+with open("File handling\\file1.txt", "+w") as a:
+
+    a.write("Hi ALAY !!!\n")
+    a.write("Hi Everyone\n")
+    a.write("Everone is good \n ")
+    print(a.readline())
+

66c8d98eae78c3721993200c39a1f53f6b6e9587
Alay <alaysakhiya525@gmail.com>
2026-09-09 15:26:11 +0530

    new

---
+++ Project/Module_packeg_Project/Project.py
@@ -0,0 +1,15 @@
+print("""
+==========================
+Welcome to Multi-utility Toolkit
+==========================
+""")
+
+
+print("""Choose an opition:
+1. Datetime and Time Operation
+2. Mathematical Operation
+3. Random Data Generation
+4. Generat Unique Identifier (UUID)
+5. File Operation
+6. Explore Module Attribute (dir())
+7. Exit """)

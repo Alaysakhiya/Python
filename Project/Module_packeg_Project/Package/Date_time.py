3cdb5f0c01ecee8316cc26ebe8286e9ace4c05c0
Alay <alaysakhiya525@gmail.com>
2026-09-11 12:02:33 +0530

    done

--- Project/Module_packeg_Project/Package/Date_time.py
+++ Project/Module_packeg_Project/Package/Date_time.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import time
 
 
 print("""
@@ -13,20 +14,54 @@ DateTime and Time Operation :
 
 while True:
 
-        sub_choice = int(input("Enter the Choice :> "))
+        sub_choice = int(input("\nEnter the Choice :> "))
 
         if sub_choice == 1:
             current_time = datetime.now()
             print(f"Current Date and Time :> {current_time}")
+
         elif sub_choice == 2:
-            pass
+
+            first_d = input("Enter the First Date (YYYY-MM-DD) : ")
+            second_d = input("Enter the Second Date (YYYY-MM-DD) : ")
+
+            date_1 = datetime.strptime(first_d,"%Y-%m-%d")
+            date_2= datetime.strptime(second_d,"%Y-%m-%d")
+
+            diffrence = (date_1 - date_2)
+
+            print(f"Difference :> {diffrence}")
+
         elif sub_choice == 3:
-            pass
+            data = input("Enter the First Date (YYYY-MM-DD) : ")
+
+            new_data = datetime.strptime(data,"%Y-%m-%d")
+            format_date = datetime.strftime(new_data,"%d/%m/%Y")
+
+            print(f"New Formated Date :> {format_date}")
+
         elif sub_choice == 4:
-            pass
+            second= int(input("Enter Second to Stop watch :> "))
+
+            for i in range (1,second +1):
+                print(i)
+                time.sleep(1)
+            print("Stop Watch is Over !")
+
         elif sub_choice == 5:
-            pass
+            second= int(input("Enter Second to CountDown :> "))
+
+            print("CountDown is Start")
+            for i in range (second,0,-1):
+                print(i)
+                time.sleep(1)
+            print("CountDownn is Over !")
+
         elif sub_choice == 6:
-            pass
+
+            print("Return To Main Manu !")
+            break
+
         else:
-            pass
+            print("Invalid Choice !")
+            break
--- practice/practice.py
+++ practice/practice.py
@@ -1,19 +1,10 @@
 
-from datetime import datetime , time
+from datetime import datetime 
+import time
 
 current_time = datetime.today()
 
 
-# print(current_time)
-# print(format_date)
 
-first_d = input("Enter the First Date (YYYY-MM-DD) : ")
-second_d = input("Enter the Second Date (YYYY-MM-DD) : ")
 
-date_1 = datetime.strptime(first_d,"%Y-%m-%d")
-date_2= datetime.strptime(second_d,"%Y-%m-%d")
-
-diffrence = (date_1 - date_2)
-
-print(f"Difference :> {diffrence}")
 
